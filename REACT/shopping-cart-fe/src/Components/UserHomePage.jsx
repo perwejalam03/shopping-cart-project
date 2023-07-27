4c69127b0c22bd789ace2083153e7bfe8b7acd89
perwejalam03 <perwejalam03@gmail.com>
2023-07-27 13:20:13 +0530

    user home page updated and added

--- REACT/shopping-cart-fe/src/Components/UserHomePage.jsx
+++ REACT/shopping-cart-fe/src/Components/UserHomePage.jsx
@@ -1,50 +1,17 @@
-import React from "react";
-import Button from "react-bootstrap/Button";
-import Container from "react-bootstrap/Container";
-import Form from "react-bootstrap/Form";
-import Nav from "react-bootstrap/Nav";
-import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import UserDashBoard from './UserDashBoard'
+import Navbar from './Navbar'
+
 const UserHomePage = () => {
   return (
     <div>
-      <Navbar expand="lg" className="bg-body-tertiary">
-        <Container fluid>
-          <Navbar.Brand href="#">Zip Kart</Navbar.Brand>
-          <Navbar.Toggle aria-controls="navbarScroll" />
-          <Navbar.Collapse id="navbarScroll">
-            <Nav
-              className="me-auto my-2 my-lg-0"
-              style={{ maxHeight: "100px" }}
-              navbarScroll
-            >
-              <Nav.Link href="#action1">Home</Nav.Link>
-              <Nav.Link href="#action2">Wishlist</Nav.Link>
-              <NavDropdown title="Profile" id="navbarScrollingDropdown">
-                <NavDropdown.Item href="#action3">Setting</NavDropdown.Item>
-                <NavDropdown.Item href="#action4">
-                  Another action
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action5">
-                  Something else here
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-            <Form className="d-flex">
-              <Form.Control
-                type="search"
-                placeholder="Search"
-                className="me-2"
-                aria-label="Search"
-              />
-              <Button variant="outline-success">Search</Button>
-            </Form>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<UserDashBoard/>}/>
+      </Routes>
     </div>
-  );
-};
+  )
+}
 
-export default UserHomePage;
+export default UserHomePage
