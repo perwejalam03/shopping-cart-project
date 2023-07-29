4432e7f768e75f1d5857a4934d010a7a3103374c
perwejalam03 <perwejalam03@gmail.com>
2023-07-29 18:20:13 +0530

    navbar jsx added

--- REACT/shopping-cart-fe/src/Components/Navbar.jsx
+++ REACT/shopping-cart-fe/src/Components/Navbar.jsx
@@ -18,7 +18,16 @@ const Navbar = () => {
                 </div>
                 <div className="profile">
                     <img src="https://media.istockphoto.com/id/1300845620/vector/user-icon-flat-isolated-on-white-background-user-symbol-vector-illustration.jpg?s=612x612&w=0&k=20&c=yBeyba0hUkh14_jgv1OKqIH0CCSWU_4ckRkAoy2p73o=" alt="" />
+                   <div className="d1">
+                   <Link to='' id='l1'>Edit Profile</Link>
+                    <Link to='' id='l2'>Check Order</Link>
+                    <Link to='' id='l3'>Update Address</Link>
+                    <Link to='' id='l4'>Logout</Link>
+                   </div>
                 </div>
+            </div>
+            <div className="hambarger">
+                <img src="https://www.svgrepo.com/show/506800/burger-menu.svg" alt="" />
             </div>
         </div>
     </div>
