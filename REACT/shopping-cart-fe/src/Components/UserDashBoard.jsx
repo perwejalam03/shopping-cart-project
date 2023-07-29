d6db0b7d7ad162ae7ea9a7827ac82847dafa651d
perwejalam03 <perwejalam03@gmail.com>
2023-07-29 18:21:52 +0530

    user dashboard added

--- REACT/shopping-cart-fe/src/Components/UserDashBoard.jsx
+++ REACT/shopping-cart-fe/src/Components/UserDashBoard.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import "../styles/userdashboard.css"
 const UserDashBoard = () => {
   return (
     <div>
-      <h1>User DashBoard</h1>
+       <div className="body">
+          <img src="https://cdn.pixabay.com/photo/2020/03/27/17/03/shopping-4974313_1280.jpg" alt="" />
+       </div>
     </div>
   )
 }
