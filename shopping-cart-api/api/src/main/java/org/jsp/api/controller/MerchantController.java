d9303cca87cca74873719cbc2e75526abe8cd393
perwejalam03 <perwejalam03@gmail.com>
2023-07-29 18:26:46 +0530

    merchant controller updated and added

--- shopping-cart-api/api/src/main/java/org/jsp/api/controller/MerchantController.java
+++ shopping-cart-api/api/src/main/java/org/jsp/api/controller/MerchantController.java
@@ -1,8 +1,10 @@
 package org.jsp.api.controller;
 
 import org.jsp.api.dto.Merchant;
+import org.jsp.api.dto.ResponseStructure;
 import org.jsp.api.service.MerchantService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,12 +21,12 @@ public class MerchantController {
 	private MerchantService service;
 
 	@PostMapping("/merchants")
-	public Merchant savMerchant(@RequestBody Merchant merchant, HttpServletRequest request) {
+	public ResponseEntity<ResponseStructure<Merchant>> savMerchant(@RequestBody Merchant merchant, HttpServletRequest request) {
 		return service.savMerchant(merchant, request);
 	}
 
 	@GetMapping("/merchants/verify")
-	public String verifyMerchant(@RequestParam String token) {
+	public ResponseEntity<ResponseStructure<String>> verifyMerchant(@RequestParam String token) {
 		return service.verifyMerchant(token);
 	}
 	
