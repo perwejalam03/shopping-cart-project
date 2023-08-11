b99cfad76b9df0d0e0d9c62780ca49bfdc66065a
perwejalam03 <perwejalam03@gmail.com>
2023-08-11 11:05:03 +0530

    merchant home page updated and added

--- REACT/shopping-cart-fe/src/Components/MerchantHomePage.jsx
+++ REACT/shopping-cart-fe/src/Components/MerchantHomePage.jsx
@@ -5,6 +5,7 @@ import {Routes,Route} from "react-router-dom"
 import FooterPage from './FooterPage'
 import AddItems from './AddItems'
 import MerchantUpdate from './MerchantUpdate'
+import Products from './Products'
 const MerchantHomePage = () => {
   return (
     <div>
@@ -12,6 +13,7 @@ const MerchantHomePage = () => {
       <Routes>
         <Route path="/" element={<MerchantDashBoard/>}/>
         <Route path="/additems" element={<AddItems/>}/>
+        <Route path='/products' element={<Products/>}/>
         <Route path="/editprofile" element={<MerchantUpdate/>}/>
       </Routes>
       <FooterPage/>
