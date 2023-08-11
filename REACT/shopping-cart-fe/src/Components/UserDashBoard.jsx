8329f2d3eb2a19ad6a17a22bb8548b6e8acfc630
perwejalam03 <perwejalam03@gmail.com>
2023-08-11 11:08:57 +0530

    userdashboard added

--- REACT/shopping-cart-fe/src/Components/UserDashBoard.jsx
+++ REACT/shopping-cart-fe/src/Components/UserDashBoard.jsx
@@ -1,13 +1,50 @@
-import React from 'react'
+import axios from "axios";
+import React from "react"
+import { useEffect, useState } from "react";
+// import "../styles/products.css"
 import "../styles/userdashboard.css"
-const UserDashBoard = () => {
-  return (
-    <div>
-       <div className="body">
-          <img src="https://cdn.pixabay.com/photo/2020/03/27/17/03/shopping-4974313_1280.jpg" alt="" />
-       </div>
-    </div>
-  )
-}
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
-export default UserDashBoard
+const UserDashBoard = () => {
+    let [products,setProducts]=useState([])
+  useEffect(() => {
+    let fetchData = () => {
+      axios.get("http://localhost:8080/products")
+        .then((res) => {
+          console.log(res.data.data);
+          setProducts(res.data.data)
+        })
+        .catch(() => {
+          alert("Not a good request");
+        });
+    };
+    fetchData();
+  },[]);
+ return(
+    <div className="view1">
+        {products.map((x)=>{
+            return(
+               <div className="mobile1">
+                <div className="display1">
+                <div className="image1">
+                  <img src={x.image} alt="" />
+                </div>
+                <hr />
+                <div className="details1">
+                  <h1>{x.brand}</h1>
+                  <span>Flat INR 2000 Off on ICICI CreditCard</span>
+                  <h2>{x.name}</h2>
+                  <p>MRP Price:<strike>{x.price}</strike></p>
+                  <button className="btn btn-danger py-0 px-5">DEAL OF THE DAY</button>
+                  <p>Discount Price:<h3 id="discount">{x.price-(x.price * 12/100)}</h3></p>
+                  <button className="btn btn-success b1">Add item</button>  <FavoriteIcon/>
+                </div>
+               </div>
+               </div>
+            )
+        })}
+    </div>
+ )
+};
+
+export default UserDashBoard;
