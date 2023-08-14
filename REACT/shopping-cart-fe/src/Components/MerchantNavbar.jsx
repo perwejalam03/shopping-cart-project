76d37e9324aee139b6e6537718c66a07f4a22d58
perwejalam03 <perwejalam03@gmail.com>
2023-08-14 18:59:23 +0530

    merchant navbar updated

--- REACT/shopping-cart-fe/src/Components/MerchantNavbar.jsx
+++ REACT/shopping-cart-fe/src/Components/MerchantNavbar.jsx
@@ -25,8 +25,7 @@ const MerchantNavbar = () => {
                    {sub && <div className="d1">
                    <Link to='/merchanthome/editprofile' id='l1'>Edit Profile</Link>
                     <Link to='' id='l2'>Check Order</Link>
-                    <Link to='' id='l3'>Update Address</Link>
-                    <Link to='' id='l4'>Logout</Link>
+                    <Link to='/' id='l4'>Logout</Link>
                    </div>}
                 </div>
             </div>
