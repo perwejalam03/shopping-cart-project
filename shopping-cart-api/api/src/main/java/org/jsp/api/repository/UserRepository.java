f2c7b9d649a5360068277a526bd62742a7542b5f
perwejalam03 <perwejalam03@gmail.com>
2023-08-16 21:41:57 +0530

    user repository updated and added

--- shopping-cart-api/api/src/main/java/org/jsp/api/repository/UserRepository.java
+++ shopping-cart-api/api/src/main/java/org/jsp/api/repository/UserRepository.java
@@ -1,8 +1,10 @@
 package org.jsp.api.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.jsp.api.dto.Merchant;
+import org.jsp.api.dto.Product;
 import org.jsp.api.dto.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,11 +13,16 @@ public interface UserRepository extends JpaRepository<User, Integer> {
 
 	@Query("select u from User u where u.token=?1")
 	public User findUserByToken(String token);
-	
+
 	@Query("select u from User u where u.email=?1")
 	public User findUserByEmail(String token);
 
 	@Query("select u from User u where u.email=?1 and u.password=?2")
-	public Optional<User> loginverifyByUser(String email,String password);
+	public Optional<User> loginverifyByUser(String email, String password);
 
+	@Query("select u.cart from User u where u.id=?1")
+	public List<Product> findProductsInCart(int id);
+
+	@Query("select u.wishList from User u where u.id=?1")
+	public List<Product> findProductsInWishList(int id);
 }
